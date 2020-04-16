f4c0068aab0a27a1f433e5739b39515d6801f623
krishna-swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-04-16 07:56:54 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -9,21 +9,21 @@ module testbench();
   // initialize test
   initial
     begin
-      reset  1; # 22; reset  0;
+      reset <= 1; # 22; reset <= 0;
     end
   // generate clock to sequence tests
   always
     begin
-      clk  1; # 5; clk  0; # 5;
+      clk <= 1; # 5; clk <= 0; # 5;
     end
   // check results
   always @ (negedge clk)
     begin
       if (memwrite) begin
-        if (dataadr  84 & writedata  7) begin
+        if (dataadr === 84 & writedata === 7) begin
           $display ("Simulation succeeded");
           $stop;
-        end else if (dataadr ! 80) begin
+        end else if (dataadr !== 80) begin
           $display ("Simulation failed");
           $stop;
         end
