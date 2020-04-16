afe5a1e679413770ef507b9bc44c007bf5e51f40
krishna-swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-04-16 08:01:59 +0530

    Update aludec.v

--- Basic/aludec.v
+++ Basic/aludec.v
@@ -6,15 +6,15 @@ module aludec( input [5:0] funct,
                output reg [2:0] alucontrol);
   always@(*)
     case(aluop)
-      2b00: alucontrol <= 3b010; // add
-      2b01: alucontrol <= 3b110; // sub
+      2'b00: alucontrol <= 3'b010; // add
+      2'b01: alucontrol <= 3'b110; // sub
       default: case(funct) // RTYPE
-                 6b100000: alucontrol <= 3b010; // ADD
-                 6b100010: alucontrol <= 3b110; // SUB
-                 6b100100: alucontrol <= 3b000; // AND
-                 6b100101: alucontrol <= 3b001; // OR
-                 6b101010: alucontrol <= 3b111; // SLT (Shift left)
-                 default: alucontrol <= 3bxxx; // ???
+                 6'b100000: alucontrol <= 3'b010; // ADD
+                 6'b100010: alucontrol <= 3'b110; // SUB
+                 6'b100100: alucontrol <= 3'b000; // AND
+                 6'b100101: alucontrol <= 3'b001; // OR
+                 6'b101010: alucontrol <= 3'b111; // SLT (Shift left)
+                 default: alucontrol <= 3'bxxx; // ???
                endcase
       endcase
 endmodule
