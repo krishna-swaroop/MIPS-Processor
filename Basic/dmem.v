32f761e4448e088521aba22f560119c4d64f846e
krishna-swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-04-16 08:04:00 +0530

    Update and rename dmem.v to Basic/dmem.v

--- Basic/dmem.v
+++ Basic/dmem.v
@@ -3,7 +3,7 @@ module dmem (input clk, we,
              input [31:0] a, wd,
              output [31:0] rd);
   reg [31:0] RAM[63:0];
-  assign rd  RAM[a[31:2]]; // word aligned
+  assign rd = RAM[a[31:2]]; // word aligned
   always @ (posedge clk)
     if (we)
       RAM[a[31:2]] <= wd;
