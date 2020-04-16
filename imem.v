86a347495e6da2308c0c1d197e204c558f1e636e
krishna-swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-04-16 07:21:37 +0530

    Create imem.v

---
+++ imem.v
@@ -0,0 +1,11 @@
+// Decription of Instruction Memory
+
+module imem (input [5:0] a,
+             output [31:0] rd);
+  reg [31:0] RAM[63:0];
+  initial
+    begin
+      $readmemh ("memfile.dat",RAM);
+    end
+  assign rd  RAM[a]; // word aligned
+endmodule
