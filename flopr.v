39bb985ea5d060393c8ba68c458e7970aeccb111
krishna-swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-04-16 06:55:57 +0530

    Create flopr.v

---
+++ flopr.v
@@ -0,0 +1,13 @@
+// Description of a resettable D-Flip-Flop
+
+module flopr # (parameter WIDTH = 8)
+               (input clk, reset,
+                input [WIDTH-1:0] d,
+                output reg [WIDTH-1:0] q);
+  
+  always @ (posedge clk, posedge reset)
+    if (reset) 
+      q <= 0;
+    else 
+      q <= d;
+endmodule
